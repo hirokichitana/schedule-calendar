c4f188ed24321044e5beafbde2bf45aeb86947a6
hirokichitana <94688508+hirokichitana@users.noreply.github.com>
2022-03-17 20:12:00 +0900

    scheduleコントローラーの編集、schedules/indexおよびschedules/newのビューの編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'schedules/index'
+  # get 'schedules/index'
   root to: 'schedules#index'
-  resources :schedules, only: :index
+  resources :schedules, only: [:index, :new, :create]
 end
