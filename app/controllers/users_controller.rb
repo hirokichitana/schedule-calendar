76ea9cc3741dfe461b52ba6e02c5618798426080
hirokichitana <94688508+hirokichitana@users.noreply.github.com>
2022-03-24 22:34:38 +0900

    userコントローラーの作成

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,7 @@
+class UsersController < ApplicationController
+  def show
+    @name = current_user.name
+    @schedules = Schedule.where(id: current_user.schedules)
+    @user = User.where(id: params[:id])
+  end
+end
