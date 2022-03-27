575bca1e3b8f3e39391b3e378d5e3e0b63ccda42
hirokichitana <94688508+hirokichitana@users.noreply.github.com>
2022-03-27 13:22:40 +0900

    コントローラの編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def show
-    @name = current_user.name
-    @schedules = Schedule.where(id: current_user.schedules)
+    user = User.find(params[:id])
+    @schedules = user.schedules
     @user = User.where(id: params[:id])
   end
 
