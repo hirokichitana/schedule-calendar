8037d0a1ddc0548427a8ea942e28bff974b2e927
hirokichitana <94688508+hirokichitana@users.noreply.github.com>
2022-04-08 21:48:13 +0900

    コメントコントローラーへデストロイアクションの記述

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,6 +4,12 @@ class CommentsController < ApplicationController
     redirect_to "/schedules/#{comment.schedule.id}"
   end
 
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    redirect_to "/schedules/#{@comment.schedule.id}"
+  end
+
   private
   def comment_params
     params.require(:comment).permit(:text).merge(user_id: current_user.id, schedule_id: params[:schedule_id])
