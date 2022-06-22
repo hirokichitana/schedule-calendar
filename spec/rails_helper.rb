7567b0bc12d80490263350449c19b9b0e27bf533
hirokichitana <94688508+hirokichitana@users.noreply.github.com>
2022-06-22 22:49:09 +0900

    rails_helperの編集

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -36,6 +36,7 @@ RSpec.configure do |config|
   config.include BasicPassSupport
   config.include ScheduleCountUpSupport
   config.include ScheduleCountNotUpSupport
+  config.include CommentCountUpSupport
   config.include CommentCountNotUpSupport
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
